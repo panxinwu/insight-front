6d43416826d169974172866f8f130dc1b5793be5
panxinwu <xinwupan@vip.qq.com>
2015-12-01 10:50:41 +0800

    instight-front first commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('insightFront')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
